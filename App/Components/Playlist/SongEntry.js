a3a708db9fed09fec96a85d9e4089863d15d5dc4
Android <plasm0dium@Androids-MacBook-Pro.local>
2015-10-08 16:12:42 -0700

    [Update] SongEntry.js

--- App/Components/Playlist/SongEntry.js
+++ App/Components/Playlist/SongEntry.js
@@ -6,54 +6,7 @@ var Firebase = require('firebase');
 
 
 var SongEntry = React.createClass({
-
-  loadSongsFromServer: function(receivedCode) {
-
-
-    this.firebaseRef = new Firebase('https://llamajamsauth.firebaseio.com/' + receivedCode + '/playlist');
-    console.log(receivedCode);
-    console.log("loading songs");
-
-    this.firebaseRef.on('child_added', function(snapshot) {
-
-      var eachSong = snapshot.val()
-      var eachTitle = eachSong.title;
-      var separateTitleandArtist = eachTitle.indexOf('-')
-
-      var artist = eachTitle.slice(0, separateTitleandArtist)
-      var song = eachTitle.slice(separateTitleandArtist + 2, eachTitle.length)
-
-        this.items.push({
-          artist: artist,
-          song: song,
-          songUrl: eachSong.songUrl
-        });
-
-      this.setState({songs: this.items})
-    }.bind(this));
-  },
-
-  rerenderPlaylist: function() {
-    this.firebaseRef.on('child_removed', function(snapshot) {
-      console.log('inside rerenderPlaylist:', snapshot.val());
-      var removeSongbyUrl = snapshot.val().songUrl;
-
-      var isFound = false;
-      for(var i = 0; i < this.state.songs.length; i++) {
-        if(this.state.songs[i].songUrl === removeSongbyUrl && !isFound) {
-          this.state.songs.splice(i,1)
-          isFound = true;
-        }
-      }
-
-      this.setState({
-        songs: this.state.songs
-      })
-
-      this.forceUpdate();
-    }.bind(this));
-  },
-
+  // Sets the initial playlist code to an empty string
   getDefaultProps: function() {
     return {
       playlistCode: ''
@@ -61,7 +14,6 @@ var SongEntry = React.createClass({
   },
 
   getInitialState: function(){
-    this.items = [];
     return {
       songs: [],
       active: false,
@@ -72,9 +24,59 @@ var SongEntry = React.createClass({
     }
   },
 
+  // This function fetches the right playlist from Firebase based on
+  // your playlist code.
+  loadSongsFromServer: function(receivedCode) {
+    this.items = [];
+    this.firebaseRef = new Firebase('https://magpiejammies.firebaseio.com/' + receivedCode + '/playlist');
+
+    this.firebaseRef.on('child_added', function(snapshot) {
+      var eachSong = snapshot.val();
+      var eachTitle = eachSong.title;
+      var eachVoteUp = eachSong.voteUp;
+      var eachVoteDown = eachSong.voteDown;
+      var eachKey = snapshot.key();
+      var eachDuration = eachSong.duration;
+      var eachImg = eachSong.artwork_url;
+      // Pushes each song into the items array for rendering
+      var eachVoteSum = eachVoteUp - eachVoteDown;
+      var duration = eachDuration;
+      var artwork_url = eachImg;
+      this.items.push({
+        song: eachTitle,
+        songUrl: eachSong.songUrl,
+        key: eachKey,
+        voteUp: eachVoteUp,
+        voteDown: eachVoteDown,
+        voteSum: eachVoteSum,
+        duration: duration,
+        artwork_url: artwork_url
+      });
+      this.setState({songs: this.items})
+    }.bind(this));
+  },
+
+  // This rerenders the playlist every time a song is removed from Firebase
+  rerenderPlaylist: function() {
+    this.firebaseRef.on('child_removed', function(snapshot) {
+      var removeSongbyUrl = snapshot.val().songUrl;
+      var isFound = false;
+      // Loops through the songs array and removes the deleted song
+      // from firebase to control rendering for react
+      for(var i = 0; i < this.state.songs.length; i++) {
+        if(this.state.songs[i].songUrl === removeSongbyUrl && !isFound) {
+          this.state.songs.splice(i,1)
+          isFound = true;
+        }
+      }
+      // Resets the state to accurately reflect removed items
+      this.setState({songs: this.state.songs});
+      this.forceUpdate();
+    }.bind(this));
+  },
+
   componentWillReceiveProps: function(nextProps) {
     this.state.hasToken = nextProps.hasToken;
-    console.log('receiving props:', nextProps.playlistCode);
     var receivedCode = nextProps.playlistCode;
     this.loadSongsFromServer(receivedCode);
     this.rerenderPlaylist();
@@ -85,7 +87,7 @@ var SongEntry = React.createClass({
       input: inputSearch
     });
     this.toggleBox();
-    this.soundCloudCall(inputSearch)
+    this.soundCloudCall(inputSearch);
   },
 
   toggleBox: function() {
@@ -94,23 +96,29 @@ var SongEntry = React.createClass({
     })
   },
 
+  // This function adds songs to firebase
   pushSong: function(e) {
-    var selectedSong = e.target.childNodes[0].data
+    console.log('dude, what is e.target? ', e.target);
+    var selectedSong = e.target.childNodes[0].data;
     var allResults = this.state.searchResults;
+    console.log('what is in those searchResults? ', allResults);
 
     for(var i = 0; i < allResults.length; i++) {
       if(allResults[i].title === selectedSong) {
         this.firebaseRef.push({
           title: allResults[i].title,
-          songUrl: allResults[i].songUrl
-        })
+          songUrl: allResults[i].songUrl,
+          voteUp: 0,
+          voteDown: 0,
+          voteSum: 0
+        });
       }
     }
-
    this.toggleBox();
    e.preventDefault();
   },
 
+  // Controls the play and pause functionality of the music player
   playPause: function() {
     var fbref = this.firebaseRef;
     var songs = this.state.songs;
@@ -119,41 +127,27 @@ var SongEntry = React.createClass({
     var myOptions = {
       onload : function() {
         var duration = this.duration;
-        console.log('loaded');
       },
       onfinish : function(){
-        //delete first song from firebase
+        // Delete first song from firebase
         var children = [];
         fbref.once('value', function(snapshot){
           snapshot.forEach(function(childSnapshot){
             children.push(childSnapshot.key().toString());
-          })
+          });
         });
         fbref.child(children[0]).remove();
-        //shift the top song off the playlist array
-
-        // play firstSong
+        // Play firstSong
         SC.stream(player.state.songs[0].songUrl, myOptions, function(song) {
+          // Put countdown here
           song.play();
-        })
-         
-      },
-      onresume : function(){
-        console.log("resumed");
-      },
-      onstop : function(){
-        console.log("Stopped");
-      },
-      onpause : function() {
-        console.log('Paused');
-      },
-      whileplaying : function() {
-        console.log(this.position);
+        });
       }
-    }
-
+    };
+    // If there's no current soundManager object, create one
     if(!window.soundManager){
       SC.stream(player.state.songs[0].songUrl, myOptions, function(song) {
+        // Put countdown here too
         song.play();
       })
     }else{
@@ -162,60 +156,99 @@ var SongEntry = React.createClass({
           toggle: false
         })
         window.soundManager.resumeAll();
-        console.log('in resumeAll')
       }else{
         this.setState({
           toggle: true
         })
         window.soundManager.pauseAll();
-        console.log('paudAll')
       }
     }
-
   },
 
+  // Controls the searching and displaying of results from the SoundCloud API
   soundCloudCall: function(inputSearch) {
     if(this.state.searchResults.length > 0) {
       this.setState({ searchResults: this.state.searchResults.slice(0) })
       this.forceUpdate();
     } 
-
     SC.get('http://api.soundcloud.com/tracks/', { q: inputSearch, limit: 7 }, function(tracks) {
     // Display each song title and an option to add '+' to host playlist
-      var obj = [];
+      var array = [];
 
       for(var i = 0; i < tracks.length; i++) {
         var eachSong = tracks[i].title;
         var eachUrl = tracks[i].uri;
-
-
-        obj.push({
+        var eachDur = tracks[i].duration;
+        var eachImg = tracks[i].artwork_url;
+        array.push({
           title: eachSong,
-          songUrl: eachUrl
+          songUrl: eachUrl,
+          duration: eachDur,
+          artwork: eachImg
+        });
+       }
+      this.setState({ 
+        searchResults: array
+      })
+   }.bind(this));
+  },
+
+  handleOnThatClickUp: function(arg) {
+    for (var i = 0; i < this.state.songs.length; i++) {
+      if (arg.key === this.state.songs[i].key) {
+        this.state.songs[i].voteUp++;
+        
+        this.firebaseRef.child(arg.key).update({
+          'voteUp': this.state.songs[i].voteUp,
+          'voteSum': this.state.songs[i].voteUp - this.state.songs[i].voteDown
         });
 
-        console.log(obj)
-       }
+        this.firebaseRef.child(arg.key).child('voteUp').on('value', function(snapshot) {
+          console.log('give me that voteUp: ', snapshot.val());
+        });
 
-      this.setState({ 
-        searchResults: obj 
-      })
+        this.firebaseRef.child(arg.key).child('voteSum').on('value', function(snapshot) {
+          console.log('hey, the voteSum went up to ', snapshot.val());
+        });
 
-   }.bind(this));
+      }
+    }
+  
+  },
+
+  handleOnThatClickDown: function(arg) {
+    for (var i = 0; i < this.state.songs.length; i++) {
+      if (arg.key === this.state.songs[i].key) {
+        this.state.songs[i].voteDown++;
+        
+        this.firebaseRef.child(arg.key).update({
+          'voteDown': this.state.songs[i].voteDown,
+          'voteSum': this.state.songs[i].voteUp - this.state.songs[i].voteDown
+        });
+
+        this.firebaseRef.child(arg.key).child('voteDown').on('value', function(snapshot) {
+          console.log('give me that voteDown: ', snapshot.val())
+        });
+
+        this.firebaseRef.child(arg.key).child('voteSum').on('value', function(snapshot) {
+          console.log('look here, the voteSum went down to ', snapshot.val());
+        });
+
+      }
+    }
+    
 
   },
 
   render: function(){
-    console.log('rendered:', this.props.playlistCode);
-    var songStructure = this.state.songs.map(function(song, i) {
-      return <Song data={song} key={i}/>
-    })
-
-    var songResults = this.state.searchResults.map(function(song, i) {
+    var context = this;
+    var songResults = this.state.searchResults.map(function(song) {
       var songUri = song.songUrl
-      return <a className='song-results' href='#' ref='eachSoundcloud' value={songUri}>{song.title}<div className='plus'>+</div></a>
-    })
-
+      return <a className='song-results' href='#' ref='eachSoundcloud' value={songUri}> {song.title} <div className='plus'>+</div></a>
+    });
+    var songStructure = this.state.songs.map(function(song) {
+      return <Song data={song} onThatClickUp={context.handleOnThatClickUp} onThatClickDown={context.handleOnThatClickDown}/>
+    });
     if(this.state.active) {
       var display = {
         display: 'block'
@@ -225,7 +258,6 @@ var SongEntry = React.createClass({
         display: 'none' 
       }
     }
-
     return (
       <div>
        {this.state.hasToken ? <Player togglePlayer={this.playPause}/> : null}
@@ -238,7 +270,6 @@ var SongEntry = React.createClass({
        {songStructure}
       </div>
     );
-    
    },
 
   componentDidMount: function() {
@@ -247,8 +278,6 @@ var SongEntry = React.createClass({
       this.rerenderPlaylist();
     }
   }
-
-
-  });
+});
 
 module.exports = SongEntry;
